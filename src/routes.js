2bd8da40fc975497f80f4b4395f04382bb81f5cd
eltonMoraisD <eltondelgadomorais@gmail.com>
2020-05-01 06:43:20 -0100

    criando loader de models

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) =>
-  res.json({
-    message: 'Hello express',
-  })
-);
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Elton',
+    email: 'elton@gmail.com',
+    password_hash: '12445323523523523523523',
+  });
+  res.json(user);
+});
 
 export default routes;
