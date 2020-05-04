6e049029574d4343287651d40b67e32beb47f9eb
eltonMoraisD <eltondelgadomorais@gmail.com>
2020-05-03 23:55:41 -0100

    listagem dos providers

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -13,6 +14,9 @@ const upload = multer(multerConfig);
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 routes.put('/users', authMiddleware, UserController.update);
+
 routes.post('/files', upload.single('file'), FileController.store);
+
+routes.get('/providers', ProviderController.index);
 
 export default routes;
