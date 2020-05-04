fdea97022706dae52b7e6b8485755f1859fe816d
eltonMoraisD <eltondelgadomorais@gmail.com>
2020-05-04 22:28:37 -0100

    validaçoes de agendamento

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,4 +1,5 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 
@@ -18,7 +19,6 @@ class AppointmentController {
     /*
      *Check if provider_id is a provider
      */
-
     const isProvider = await User.findOne({
       where: { id: provider_id, provider: true },
     });
@@ -29,10 +29,40 @@ class AppointmentController {
       });
     }
 
+    const hourStart = startOfHour(parseISO(date));
+
+    /* testa se a data que o usuario passou esta antes da data atual, ou seja,
+     * uma data que ja passou
+     */
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({
+        message: 'Past dates are not permitted',
+      });
+    }
+
+    /*
+     * Verificar se o prestador de serviço ja tem um agendamento marcado para a data que,
+     * o usuario digitou
+     */
+
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        cancelled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (checkAvailability) {
+      return res.status(400).json({
+        message: 'Appointment date is not available',
+      });
+    }
+
     const appointments = await Appointment.create({
       user_id: req.userId,
       provider_id,
-      date,
+      date: hourStart,
     });
 
     return res.status(200).json(appointments);
