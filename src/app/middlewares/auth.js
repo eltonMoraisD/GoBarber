49a166395019f11ef27b8c259d8cbb9a81aba6ef
eltonMoraisD <eltondelgadomorais@gmail.com>
2020-05-04 01:46:54 -0100

    Agendamento de serviço

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -17,7 +17,7 @@ export default async (req, res, next) => {
   try {
     const decoded = await promisify(jwt.verify)(token, authConfig.secret);
 
-    req.userId = decoded.id; //  inclui o id na requisiçao
+    req.userId = decoded.id; //  inclui o id na requisiçao quando o usuario faz o login
 
     return next();
   } catch (err) {
--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -16,6 +17,8 @@ routes.post('/sessions', SessionController.store);
 routes.put('/users', authMiddleware, UserController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
+
+routes.post('/appointments', AppointmentController.store);
 
 routes.get('/providers', ProviderController.index);
 
